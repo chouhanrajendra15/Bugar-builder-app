d74cfa0dfa5d60a62d35b77e2c51984e16e0557d
rajendra <15rajendrachouhan@gmail.com>
2022-09-25 21:54:16 +0530

    Removing Ingredients safely

--- src/Components/Burgar/BuildControls/BuildControls.js
+++ src/Components/Burgar/BuildControls/BuildControls.js
@@ -20,7 +20,10 @@ const BuildControls = (props) => (
     <BuildControl 
     key ={ctrl.label}
      label ={ctrl.label}
-      added = {() => props.ingredientAdded(ctrl.type)} />
+      added = {() => props.ingredientAdded(ctrl.type)} 
+      removed = {() => props.ingredientRemoved(ctrl.type)}
+      disabled = {props.disabled[ctrl.type]}
+      />
   
   ))}
 
--- src/Containers/BurgarBuilder/BurgarBuilder.js
+++ src/Containers/BurgarBuilder/BurgarBuilder.js
@@ -42,18 +42,47 @@ addIngredientHandler = (type) => {
    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 }
 
+removeIngredientHandler = (type) => {
+const oldCount = this.state.ingredients[type];
+if (oldCount <= 0){
+  return ;
+}
+const updatedCount = oldCount - 1;
+const updatedIngredients = {
+  ...this.state.ingredients
+};
+updatedIngredients[type] = updatedCount;
+const priceDeduction = INGREDIENT_PRICES[type];
+const oldPrice = this.state.totalPrice;
+const newPrice = oldPrice - priceDeduction;
+this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+ 
 
+
+
+
+}
 
 
 
 
 
   render() {
+   const disabledInfo ={
+    ...this.state.ingredients
+   };
+   for ( let key in disabledInfo) {
+    disabledInfo[key] = disabledInfo[key] <= 0
+   }
+
+// {salad: true, meat: false, ...}
     return (
      <AAux> 
        <Burgar ingredients = { this.state.ingredients}  />
      <BuildControls 
       ingredientAdded = { this.addIngredientHandler}
+      ingredientRemoved = { this.removeIngredientHandler}
+      disabled = {disabledInfo}
        />
       </AAux>
     );
